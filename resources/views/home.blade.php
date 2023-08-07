370d0341db150a8a961bd392fadea63d4605176e
samira <samira@gmail.com>
2023-08-07 22:16:06 +0600

    one file deleted and homepage created

---
+++ resources/views/home.blade.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Home Page</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+		<div class="container">
+			<a class="navbar-brand" href="#"><span class="text-warning"><h3><b>UNITED INTERNATIONAL UNIVERSITY</b></h3></span></a> <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button> <br>
+
+             <br>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<br>
+				<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link" href="#"><b>Home</b></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#about"><b>About</b></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#services"><b>Research</b></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#portfolio"><b>Portfolio</b></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#team"><b>Club</b></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contact"><b>Contact</b></a>
+					</li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <b>Login</b>
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <a class="dropdown-item" href="adminlogin"><b>Admin</b></a>
+                          <a class="dropdown-item" href="teacherlogin"><b>Teacher</b></a>
+                          <a class="dropdown-item" href="studentlogin"><b>Student</b></a>
+                        </div>
+                      </li>
+
+
+
+					  <!-- <li class="nav-item">
+						<a class="nav-link text-white " href="counsilling.php">
+	
+							<div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+								<i class="material-icons opacity-10">table_view</i>
+							</div>
+	
+							<span class="nav-link-text ms-1">COUNSELLING</span>
+						</a>
+					</li>
+	 -->
+
+
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	</div><!-- about section starts -->
+	<section class="about section-padding" id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-12 col-12">
+					<div class="about-img"><img alt="" class="img-fluid" src="img/about.jpg"></div>
+				</div>
+
+				<iframe width="1200" height="465" src="https://www.youtube.com/embed/LlbljkO9iy8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>dfdf</iframe>
+
+				<div class="col-lg-8 col-md-12 col-12 ps-lg-5 mt-md-5">
+					<div class="about-text">
+						<h2>DO YOU KNOW?</h2>
+						<p><b>Every Trimester 15% students get 25%-100% tuition fee waiver based on previous results and trimester results. UIU provides about 10 to 12 crore taka as scholarship in every year, one of the highest amongst the private university. If you are a committed student, take the challenge and build your future with UIU.</b></p><a class="btn btn-warning" href="#">Learn More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- about section Ends -->
+	<!-- services section Starts -->
+	<section class="services section-padding" id="services">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-header text-center pb-5">
+						<h2>UIU Research Grant 2016</h2>
+						<p>List of Research Projects 
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-md-12 col-lg-4">
+					<div class="card text-white text-center bg-dark pb-2">
+						<div class="card-body">
+							<!-- <i class="bi bi-journal "></i> -->
+							<img class="img-account-profile rounded-circle mb-0.5" src="image/p1.jpg" alt="">
+							<h3 class="card-title">Prof. Dr. Hasnan Ahmed, Professor – School of Business & Economics</h3>
+							<p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci eligendi modi temporibus alias iste. Accusantium?
+								<br>
+								<br>
+								<br>
+								<br>
+							</p><button class="btn bg-warning text-dark">Read More</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-12 col-lg-4">
+					<div class="card text-white text-center bg-dark pb-2">
+						<div class="card-body">
+							<!-- <i class="bi bi-journal"></i> -->
+							<img class="img-account-profile rounded-circle mb-0.5" src="image/p2.jpg" alt="">
+							<h3 class="card-title">Dr. Mohd Hasanur Raihan Joarder, Associate Professor</h3>
+							<p class="lead">Quality education and employee retention practices among Private Universities in Bangladesh: Mediating effects of organization commitment (OC) and perceived organizational support (POS) on HRM practices and employee retention</p><button class="btn bg-warning text-dark">Read More</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-12 col-lg-4">
+					<div class="card text-white text-center bg-dark pb-2">
+						<div class="card-body">
+							<!-- <i class="bi bi-journal"></i> -->
+							<img class="img-account-profile rounded-circle mb-0.5" src="image/p3.jpg" alt="">
+							<h3 class="card-title">Dr. Mohammad Badruddoza Mia, Asst. Professor</h3>
+							<p class="lead">An Assessment of Impact of Social Performance of Microfinance Organizations on the Financial Performance of Microfinance Ventures
+								<br>
+								<br>
+								<br>
+								<br>
+								<br>
+								<br>
+								<br>
+								
+							</p><button class="btn bg-warning text-dark">Read More</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- services section Ends -->
+	<!-- portfolio strats -->
+	<section class="portfolio section-padding" id="portfolio">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-header text-center pb-5">
+						<h2>Our Projects</h2>
+						<p>UIU MARS ROVER TEAM SELECTED FOR THE URC 2022</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-md-12 col-lg-4">
+					<div class="card text-light text-center bg-white pb-2">
+						<div class="card-body text-dark">
+							<div class="img-area mb-4"><img alt="" class="img-fluid" src="img/project-1.jpg"></div>
+							<h3 class="card-title">UIU MARS ROVER </h3>
+							<p class="lead">The UIU Mars Rover team from United International University (UIU) has been selected in the University Rover Challenge 2022 (URC 2022) organized by The Mars Society which is the most Prestigious Robotics competition for undergraduate students.
+								<button class="btn bg-warning text-dark">Learn More</button>
+
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-12 col-lg-4">
+					<div class="card text-light text-center bg-white pb-2">
+						<div class="card-body text-dark">
+							<div class="img-area mb-4"><img alt="" class="img-fluid" src="img/project-2.jpg"></div>
+							<h3 class="card-title">Strong Ties</h3>
+							<p class="lead">This project has 6 European and 11 Asian partners. 9 Bangladeshi students, faculties and staffs went to different European countries.Prof. Dr. M. Rezwan Khan, Vice Chancellor
+								& Professor – EEE Dept., UIU <br> <br> </p><button class="btn bg-warning text-dark">learn More</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-12 col-lg-4">
+					<div class="card text-light text-center bg-white pb-2">
+						<div class="card-body text-dark">
+							<div class="img-area mb-4"><img alt="" class="img-fluid" src="img/project-3.jpg"></div>
+							<h3 class="card-title">LEADERS</h3>
+							<p class="lead">There are 10 European and 10 Asian partners. It is an ongoing project still in its initial phase. A total of 15 Bangladeshi students, faculties and staffs expected to get opportunity to visit/study in Europe. <br> <br> </p><button class="btn bg-warning text-dark">Learn More</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- portfolio ends -->
+	<!-- team starts -->
+	<section class="team section-padding" id="team">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-header text-center pb-5">
+						<h2>UIU Club</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur<br>
+						adipisicing elit. Non, quo.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12 col-md-6 col-lg-3">
+					<div class="card text-center">
+						<div class="card-body">
+							<img alt="" class="img-fluid rounded-circle" src="img/team-1.jpg">
+							<h3 class="card-title py-2">App Forum</h3>
+							<p class="card-text">This forum is a fresh initiative for mobilizing UIU campus to envision and develop a revolutionary app and service to enhance Bangladesh and our lifestyle. It serves as a platform to bring App Ideas, Machine learning, Web Development, Experience Design, Programming and Marketing on the same table.  <br>  </p>
+							<p class="socials"><i class="bi bi-twitter text-dark mx-1"></i> <i class="bi bi-facebook text-dark mx-1"></i> <i class="bi bi-linkedin text-dark mx-1"></i> <i class="bi bi-instagram text-dark mx-1"></i></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-6 col-lg-3">
+					<div class="card text-center">
+						<div class="card-body">
+							<img alt="" class="img-fluid rounded-circle" src="img/team-2.jpg">
+							<h3 class="card-title py-2">UIU Computer Club</h3>
+							<p class="card-text">Our objectives are to widen the scope of our members to make them prepare for the competitive software & IT industry. We aim to impart knowledge that is not usually covered in mainstream areas therefore it will be beneficial to our members.  <br>  <br> </p>
+							<p class="socials"><i class="bi bi-twitter text-dark mx-1"></i> <i class="bi bi-facebook text-dark mx-1"></i> <i class="bi bi-linkedin text-dark mx-1"></i> <i class="bi bi-instagram text-dark mx-1"></i></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-6 col-lg-3">
+					<div class="card text-center">
+						<div class="card-body">
+							<img alt="" class="img-fluid rounded-circle" src="img/team-3.jpg">
+							<h3 class="card-title py-2">SIAS</h3>
+							<p class="card-text">Students’ International Affairs Society (SIAS) is a dynamic student platform under the Excellency of Centre for International Affairs and Cooperation (CIAC, UIU) Our mission is to develop Entrepreneurship skills among our students with a view to synchronize between imagination & knowledge. <br> <br> <br>  </p>
+							<p class="socials"><i class="bi bi-twitter text-dark mx-1"></i> <i class="bi bi-facebook text-dark mx-1"></i> <i class="bi bi-linkedin text-dark mx-1"></i> <i class="bi bi-instagram text-dark mx-1"></i></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-6 col-lg-3">
+					<div class="card text-center">
+						<div class="card-body">
+							<img alt="" class="img-fluid rounded-circle" src="img/team-4.jpg">
+							<h3 class="card-title py-2">UIU EDF</h3>
+							<p class="card-text">UIU Career Counseling Center is a mechanism of UIU to competent Graduates for the Business World.Our vision is to develop prudential entrepreneurs who can compete in this high-tech competitive world & by breaking dogma they will be able to create their own reality & pull their own stream.</p>
+							<p class="socials"><i class="bi bi-twitter text-dark mx-1"></i> <i class="bi bi-facebook text-dark mx-1"></i> <i class="bi bi-linkedin text-dark mx-1"></i> <i class="bi bi-instagram text-dark mx-1"></i></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- team ends -->
+	<!-- Contact starts -->
+	<section class="contact section-padding" id="contact">
+		<div class="container mt-5 mb-5">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="section-header text-center pb-5">
+						<h2>Contact Us</h2>
+						<p> info@uiu.ac.bd
+							+88 09604-848-848
+                           <br>
+                           Admissions Office
+                         +8801759039498, +8801759039465, +8801759039451, +8801914001470
+                           admissions@uiu.ac.bd
+						   <br>
+                           
+                           Exam Controller’s Office
+                           examcon@uiu.ac.bd
+                           <br>
+                           Dept. of CSE
+                           monir@cse.uiu.ac.bd
+                          <br>
+                           Dept. of EEE
+                           liton@eee.uiu.ac.bd
+                           <br>
+                           Dept. of Civil Engineering
+                           rashid@admin.uiu.ac.bd
+                           School of Business & Economics
+                           <br>
+		</p>
+					</div>
+				</div>
+			</div>
+			<div class="row m-0">
+				<div class="col-md-12 p-0 pt-4 pb-4">
+					<!--for getting the form download the code from download button-->
+				</div>
+			</div>
+		</div>
+	</section><!-- contact ends -->
+	<!-- footer starts -->
+	<footer class="bg-dark p-2 text-center">
+		<div class="container">
+			<p class="text-white">All Right Reserved By team big_O</p>
+		</div>
+	</footer>
+	<!-- footer ends -->
+	
+	<!-- All Js -->
+	<script src="js/bootstrap.bundle.min.js"></script> 
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<!--divinectorweb.com-->
+<head>
+	<meta charset="UTF-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<title> </title>
+	<!-- All CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+	<link href="hs.css" rel="stylesheet">
+</head>
+<body>
+	
+</body>
+</html>
