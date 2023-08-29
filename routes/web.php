da218995dbdf27811cf0865ee9d138eea095f129
nazmul224 <nhoda201224@gmail.com>
2023-08-30 00:28:43 +0600

    admin route add

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,44 @@ Route::get('/student_profile', function () {
 });
 Route::get("/applyGbtn/{id}",[StudentController::class,"applyGrader"]);
 Route::get("/deleteGbtn/{id}",[StudentController::class,"deleteGrader"]);
+
+
+
+
+
+
+
+
+
+
+
+Route::get("/admin_grader_request",[AdminDashboardController::class,"grader_request"]);
+Route::get("/admin_ta_request",[AdminDashboardController::class,"ta_request"]);
+Route::get("/admin_ta_request",[AdminDashboardController::class,"ta_apply_data_show"]);
+Route::get("/admin_ta",[AdminDashboardController::class,"ta"]);
+Route::get("/admin_ta", [AdminDashboardController::class, "showAdminTA"]);
+Route::get("/admin_ta",[AdminDashboardController::class,"ta_list"]);
+Route::get("/admin_grader",[AdminDashboardController::class,"grader"]);
+Route::get("/admin_grader", [AdminDashboardController::class, "showAdminGrader"]);
+Route::get("/admin_grader",[AdminDashboardController::class,"grader_list"]);
+Route::get("/admin_grader_request",[AdminDashboardController::class,"grader_apply_data_show"]);
+Route::post('/update-situationta/{id}', [AdminDashboardController::class, 'updateSituationta'])->name('update.situationta');
+Route::post('/update-situationgrader/{id}', [AdminDashboardController::class, 'updateSituationgrader'])->name('update.situationgrader');
+
+
+Route::post('/adminlogin2', function (Request $request) {
+    $username = $request->input('username');
+    $password = $request->input('password');
+
+    // Replace with your desired admin credentials
+    $adminUsername = 'admin';
+    $adminPassword = '1234';
+    if ($username === $adminUsername && $password === $adminPassword) {
+        // Admin is verified, store the admin's status in the session
+        $request->session()->put('isAdminLoggedIn', true);
+        return redirect('/admin_dashboard');
+    }
+
+    // If login fails, redirect back to the login page with an error message
+    return redirect('/adminlogin2')->with('error', 'Invalid credentials.');
+});
